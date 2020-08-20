9d6329de9282736a4ce66d2e106207770ad97e21
charlescqian <charlesqian.ubc@gmail.com>
2020-08-19 19:32:04 -0700

    Added a recurring task to auto fetch given every x hours, however, only one task can run atm, cannot be used in multiple channels, added to-do to fix in the future

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os # standard library
 
 import discord # 3rd party packages 
-from discord.ext import commands 
+from discord.ext import commands, tasks
 import praw 
 from dotenv import load_dotenv
 
@@ -65,12 +65,61 @@ async def fetch(ctx, *args):
 
     sub_str = ""
     for submission in submission_list:
-        sub_str += f'{submission.score} points {submission.title}  {submission.url} \n\n '
+        sub_str += f'{submission.score} points | {submission.title} | {submission.url} \n\n '
 
-    ret_str = f"Here are the 5 {args[1]} posts on {args[0]}: \n {sub_str}" 
+    ret_str = f"Here are the 5 {args[1].lower()} posts on {args[0]}: \n {sub_str}" 
     await ctx.send(ret_str)
+
+# TODO: Currently, this only works for one instance, so perhaps creating a separate cog for each call of the command is the right way to do it
+@client.command(aliases=['auto'])
+async def fetch_auto(ctx, *args):
+
+    fetch_loop.change_interval(hours=float(args[2]))
+    fetch_loop.start(ctx.channel, args[0], args[1])
+    #auto_fetch_cog = AutoFetchCog(args[0], args[1], args[2])
+
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Your ping is {round(client.latency * 1000)}ms')
+
+@tasks.loop(hours=1)
+async def fetch_loop(channel, sr, sort_type):
+    submission_list = []
+
+    if sort_type.lower() == 'new':
+        for submission in reddit.subreddit(sr).new(limit=5):
+            submission_list.append(submission)
+    elif sort_type.lower() == 'top':
+        for submission in reddit.subreddit(sr).top(limit=5):
+            submission_list.append(submission)
+    elif sort_type.lower() == 'hot':
+        for submission in reddit.subreddit(sr).hot(limit=5):
+            submission_list.append(submission)
+    elif sort_type.lower() == 'rising':
+        for submission in reddit.subreddit(sr).rising(limit=5):
+            submission_list.append(submission)
+    
+    sub_str = ""
+    for submission in submission_list:
+        sub_str += f'{submission.score} points | {submission.title} | {submission.url} \n\n '
+
+    ret_str = f"Here are the 5 {sort_type.lower()} posts on {sr}: \n {sub_str}" 
+
+    await channel.send(ret_str)
+
+
+# class AutoFetchCog(commands.Cog):
+#     def __init__(self, subreddit, sort_type, period):
+#         self.subreddit = subreddit
+#         self.sort_type = sort_type
+#         self.period = period
+#         self.printer.start()
+
+#     @tasks.loop(seconds=10)
+#     async def printer(self):
+#         print(self.subreddit)
+#         print(self.sort_type)
+#         print(self.period)
+
 
 client.run(TOKEN)
