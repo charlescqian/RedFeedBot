24449671c1aa356e6769cc08ca1ab00d5b17ceca
charlescqian <charlesqian.ubc@gmail.com>
2020-08-27 15:47:45 -0700

    Added help documentation, changed .ping into a hidden command

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 
 from loop import MyLoop #local modules
 
+######################## Setup ###########################
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client_id = os.getenv('CLIENT_ID')
@@ -23,67 +24,67 @@ reddit = praw.Reddit(client_id=client_id,
 
 bot = commands.Bot(command_prefix='.')
 
+########################## Bot Commands ##########################
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
-# TODO: Write help documentations
-@bot.command()
-async def fetch(ctx, sr, st):
-    ret_str = __gen_ret_str(sr, st)
+@bot.command(description='Fetch 5 posts from the given subreddit, sorted by the given sort type (hot/new/top/rising). Example: \".fetch funny hot\" will fetch the 5 hottest posts from r/funny.')
+async def fetch(ctx, subreddit, sort_type):
+    ret_str = __gen_ret_str(subreddit, sort_type)
     await ctx.send(ret_str)
 
-# TODO: Write help documentations
-@bot.command(name='auto')
-async def fetch_auto(ctx, sr, st, interval):
-    loop = MyLoop(ctx.channel, sr, st, interval, __gen_ret_str)
+@bot.command(name='auto', description='Automatically fetch 5 posts from the given subreddit, sorted by the given sort type (hot/new/top/rising), at every given interval (in hours). Example: \".auto funny hot 1\" will fetch the 5 hottest posts from r/funny every hour.')
+async def fetch_auto(ctx, subreddit, sort_type, interval):
+    loop = MyLoop(ctx.channel, subreddit, sort_type, interval, __gen_ret_str)
 
-# TODO: Write help documentations
-@bot.command()
-async def feed(ctx, sr):
+@bot.command(description='Fetches the 5 newest posts from the given subreddit, then every time a new post is submitted to the subreddit, a message will be sent with the post\'s details. Example: \".feed funny\"')
+async def feed(ctx, subreddit):
 
-    ret_str = __gen_ret_str(sr, 'new')
+    ret_str = __gen_ret_str(subreddit, 'new')
     await ctx.send(ret_str)
     
     count = 0
-    sub = await async_reddit.subreddit(sr)
+    sub = await async_reddit.subreddit(subreddit)
     async for submission in sub.stream.submissions():
         if count < 100:
             count += 1
             continue
-        sub_str = f'Here is the latest post on {sr}: {submission.score} points | {submission.title} | {submission.url}'
+        sub_str = f'Here is the latest post on {subreddit}: {submission.score} points | {submission.title} | {submission.url}'
         await ctx.send(sub_str)
 
-# TODO: Write help documentations
-@bot.command()
+@bot.command(hidden='true')
 async def ping(ctx):
     await ctx.send(f'Your ping is {round(bot.latency * 1000)}ms')
 
+
+############################ Helper Functions #################################
 # TODO: Find a way to better format the strings, maybe add some hyperlinks rather then posting the whole link
-def __gen_ret_str(sr, st):
+def __gen_ret_str(subreddit, sort_type):
     submission_list = []
 
-    if st.lower() == 'new':
-        for submission in reddit.subreddit(sr).new(limit=5):
+    if sort_type.lower() == 'new':
+        for submission in reddit.subreddit(subreddit).new(limit=5):
             submission_list.append(submission)
-    elif st.lower() == 'top':
-        for submission in reddit.subreddit(sr).top(limit=5):
+    elif sort_type.lower() == 'top':
+        for submission in reddit.subreddit(subreddit).top(limit=5):
             submission_list.append(submission)
-    elif st.lower() == 'hot':
-        for submission in reddit.subreddit(sr).hot(limit=5):
+    elif sort_type.lower() == 'hot':
+        for submission in reddit.subreddit(subreddit).hot(limit=5):
             submission_list.append(submission)
-    elif st.lower() == 'rising':
-        for submission in reddit.subreddit(sr).rising(limit=5):
+    elif sort_type.lower() == 'rising':
+        for submission in reddit.subreddit(subreddit).rising(limit=5):
             submission_list.append(submission)
     
     sub_str = ""
     for submission in submission_list:
         sub_str += f'{submission.score} points | {submission.title} | {submission.url} \n\n '
 
-    ret_str = f"Here are the 5 {st.lower()} posts on {sr}: \n {sub_str}" 
+    ret_str = f"Here are the 5 {sort_type.lower()} posts on {subreddit}: \n {sub_str}" 
 
     return ret_str
 
 # TODO: Maybe a copmmand to get comments from a post? 
+
 
 bot.run(TOKEN)
