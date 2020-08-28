4c9b2d6b9c7a69f7e7f77b34cb4071bed987ffa1
charlescqian <charlesqian.ubc@gmail.com>
2020-08-28 15:34:50 -0700

    Fixed bug in __gen_embed with hot and rising

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from discord.ext import commands, tasks
 import praw
 import asyncpraw 
 from dotenv import load_dotenv
+import pymongo
 
 from loop import FetchLoop #local modules
 
@@ -16,6 +17,8 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 user_agent = os.getenv('USER_AGENT')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
 async_reddit = asyncpraw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      user_agent=user_agent)
@@ -23,14 +26,23 @@ reddit = praw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      user_agent=user_agent)
 
+client = discord.Client()
 bot = commands.Bot(command_prefix='.')
 
+# client = pymongo.MongoClient(f'mongodb+srv://dbAdmin:{DB_PASSWORD}@redfeed.goti6.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority')
+# db = client.subscriptions
+
 FETCH_LIMIT = 5
+
 ########################## Bot Events ##########################
+# TODO: On bot ready, check the DB for any channels that are subscribed 
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
+    # channel = client.get_channel(745410258142363688)
+    # await channel.send('hello')
 
+# TODO: Add more error processing
 @bot.event
 async def on_command_error(ctx, error):
     print(error)
@@ -40,22 +52,18 @@ async def on_command_error(ctx, error):
         await ctx.send(f'You are missing the **{error.param}** argument. Please pass in all required arguments, see **.help {ctx.command}** for more information.')
 
 ########################## Bot Commands ##########################
-# TODO: Add error processing, ie. when the user misses an argument in the commands
+
 @bot.command(description=f'Fetch {FETCH_LIMIT} posts from the given subreddit, sorted by the given sort type (hot/new/top/rising). Example: ".fetch funny hot" will fetch the {FETCH_LIMIT} hottest posts from r/funny.')
 async def fetch(ctx, subreddit, sort_type):
     embed = __gen_embed(subreddit, sort_type)
     await ctx.send(embed=embed)
 
-@fetch.error
-async def fetch_error(ctx, error):
-    pass
 
-# TODO: Add error processing, ie. when the user misses an argument in the commands
 @bot.command(name='auto', description='Automatically fetch {FETCH_LIMIT} posts from the given subreddit, sorted by the given sort type (hot/new/top/rising), at every given interval (in hours). Example: ".auto funny hot 1" will fetch the {FETCH_LIMIT} hottest posts from r/funny every hour.')
 async def auto(ctx, subreddit, sort_type, interval : float):
     loop = FetchLoop(ctx.channel, subreddit, sort_type, interval, __gen_embed)
 
-# TODO: Add error processing, ie. when the user misses an argument in the commands
+
 @bot.command(description='Fetches the {FETCH_LIMIT} newest posts from the given subreddit, then every time a new post is submitted to the subreddit, a message will be sent with the post\'s details. Example: ".feed funny"')
 async def feed(ctx, subreddit):
     embed = __gen_embed(subreddit, 'new')
@@ -75,6 +83,7 @@ async def feed(ctx, subreddit):
 async def ping(ctx):
     await ctx.send(f'Your ping is {round(bot.latency * 1000)}ms')
 
+# TODO: Maybe a command to get comments from a post? 
 
 ############################ Helper Functions #################################
 # TODO: Find a way to better format the strings, maybe add some hyperlinks rather then posting the whole link
@@ -112,10 +121,10 @@ def __gen_embed(subreddit, sort_type):
         for submission in reddit.subreddit(subreddit).top(limit=FETCH_LIMIT):
             submission_list.append(submission)
     elif sort_type.lower() == 'hot':
-        for submission in reddit.subreddit(subreddit).hot(limit={FETCH_LIMIT}):
+        for submission in reddit.subreddit(subreddit).hot(limit=FETCH_LIMIT):
             submission_list.append(submission)
     elif sort_type.lower() == 'rising':
-        for submission in reddit.subreddit(subreddit).rising(limit={FETCH_LIMIT}):
+        for submission in reddit.subreddit(subreddit).rising(limit=FETCH_LIMIT):
             submission_list.append(submission)
 
     embed = discord.Embed(title=f'{FETCH_LIMIT} {sort_type.lower()} posts on {subreddit}')
@@ -123,8 +132,6 @@ def __gen_embed(subreddit, sort_type):
         embed.add_field(name=submission.title, value=f'{submission.score} points | [Link]({submission.url}) | [Comments](https://www.reddit.com/r/{subreddit}/comments/{submission.id}) | Submitted at {time.ctime(submission.created_utc)}', inline=False)
     
     return embed
-
-# TODO: Maybe a command to get comments from a post? 
 
 
 bot.run(TOKEN)
